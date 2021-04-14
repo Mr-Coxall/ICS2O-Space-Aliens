99a9ff14efa5a385f562e8e1a4eb4996eb433380
Patrick Coxall <mr.coxall@mths.ca>
2021-04-14 11:41:51 +0000

    game.js added

---
+++ js/game.js
@@ -0,0 +1,11 @@
+/* global Phaser */
+
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  backgroundColor: 0x5f6e7a
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
