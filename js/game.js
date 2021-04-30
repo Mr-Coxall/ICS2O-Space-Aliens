d752a692e729dbbe3bebba1d5f6a8c60defa17cc
Patrick Coxall <mr.coxall@mths.ca>
2021-04-30 16:14:27 +0000

    added in splash image

--- js/game.js
+++ js/game.js
@@ -25,7 +25,7 @@ const config = {
     }
   },
   // set background color
-  backgroundColor: 0x5f6e7a,
+  backgroundColor: 0xffffff,
   scale: {
     mode: Phaser.Scale.FIT,
     // we place it in the middle of the page.
--- js/splashScene.js
+++ js/splashScene.js
@@ -17,13 +17,19 @@ class SplashScene extends Phaser.Scene {
 
   preload () {
     console.log('Splash Scene')
+		this.load.image('splashSceneBackground', './assets/splashSceneImage.png')
   }
 
   create (data) {
+		this.splashSceneBackgroundImage = this.add.sprite(0, 0, 'splashSceneBackground')
+		this.splashSceneBackgroundImage.x = 1920 / 2
+		this.splashSceneBackgroundImage.y = 1080 / 2
   }
 
   update (time, delta) {
-    this.scene.switch('titleScene')
+		if (time > 3000) {
+			this.scene.switch('titleScene')
+		}
   }
 }
 
