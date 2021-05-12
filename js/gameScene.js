4f22fe0e2378b00b3845d679c4aadba07da96010
Patrick Coxall <mr.coxall@mths.ca>
2021-05-12 13:52:21 +0000

    lint fix

--- js/gameScene.js
+++ js/gameScene.js
@@ -24,14 +24,13 @@ class GameScene extends Phaser.Scene {
     // images
     this.load.image('starBackground', 'assets/starBackground.png')
     this.load.image('ship', 'assets/spaceShip.png')
-    
   }
 
   create (data) {
     this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
     this.background.setOrigin(0, 0)
 
-    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')  
+    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
   }
 
   update (time, delta) {
--- js/menuScene.js
+++ js/menuScene.js
@@ -10,8 +10,8 @@ class MenuScene extends Phaser.Scene {
   constructor () {
     super({ key: 'menuScene' })
 
-		this.menuSceneBackgroundImage = null
-		this.startButton = null
+    this.menuSceneBackgroundImage = null
+    this.startButton = null
   }
 
   init (data) {
@@ -20,12 +20,13 @@ class MenuScene extends Phaser.Scene {
 
   preload () {
     console.log('Menu Scene')
-		this.load.image('menuSceneBackground', 'assets/aliens_screen_image2.jpg')
-		this.load.image('startButton', 'assets/start.png')
+
+    this.load.image('menuSceneBackground', 'assets/aliens_screen_image2.jpg')
+    this.load.image('startButton', 'assets/start.png')
   }
 
   create (data) {
-		this.menuSceneBackgroundImage = this.add.sprite(0, 0, 'menuSceneBackground')
+    this.menuSceneBackgroundImage = this.add.sprite(0, 0, 'menuSceneBackground')
     this.menuSceneBackgroundImage.x = 1920 / 2
     this.menuSceneBackgroundImage.y = 1080 / 2
 
