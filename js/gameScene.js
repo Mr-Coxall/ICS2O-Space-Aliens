73d9af54aabf9c608bb93c917138bc526dc8fa81
Patrick Coxall <mr.coxall@mths.ca>
2021-05-12 02:41:10 +0000

    lesson #8

--- js/gameScene.js
+++ js/gameScene.js
@@ -9,6 +9,9 @@
 class GameScene extends Phaser.Scene {
   constructor () {
     super({ key: 'gameScene' })
+
+    this.background = null
+    this.ship = null
   }
 
   init (data) {
@@ -17,9 +20,18 @@ class GameScene extends Phaser.Scene {
 
   preload () {
     console.log('Game Scene')
+
+    // images
+    this.load.image('starBackground', 'assets/starBackground.png')
+    this.load.image('ship', 'assets/spaceShip.png')
+    
   }
 
   create (data) {
+    this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
+    this.background.setOrigin(0, 0)
+
+    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')  
   }
 
   update (time, delta) {
