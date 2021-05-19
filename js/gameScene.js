93495164a2a69a48fc394d83f8557ba9e05e9120
Mr. Coxall <>
2021-05-19 01:53:43 +0000

    lesson 11

--- js/gameScene.js
+++ js/gameScene.js
@@ -26,6 +26,8 @@ class GameScene extends Phaser.Scene {
     this.load.image('starBackground', 'assets/starBackground.png')
     this.load.image('ship', 'assets/spaceShip.png')
     this.load.image('missile', 'assets/missile.png')
+    // sound
+    this.load.audio('laser', 'assets/laser1.wav')
   }
 
   create (data) {
@@ -65,12 +67,20 @@ class GameScene extends Phaser.Scene {
         this.fireMissile = true
         const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, 'missile')
         this.missileGroup.add(aNewMissile)
+        this.sound.play('laser')
       }
     }
 
     if (keySpaceObj.isUp === true) {
       this.fireMissile = false
     }
+
+    this.missileGroup.children.each(function (item) {
+      item.y = item.y - 15
+      if (item.y < 0) {
+        item.destroy()
+      }
+    })
   }
 }
 
