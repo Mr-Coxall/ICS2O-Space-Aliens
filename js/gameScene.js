17403782bd53463ffaa303c0e8960db4a8c8a803
Mr. Coxall <>
2021-05-28 03:17:53 +0000

    lesson 13

--- js/gameScene.js
+++ js/gameScene.js
@@ -40,6 +40,7 @@ class GameScene extends Phaser.Scene {
     this.load.image('alien', 'assets/alien.png')
     // sound
     this.load.audio('laser', 'assets/laser1.wav')
+    this.load.audio('explosion', 'assets/barrelExploding.wav')
   }
 
   create (data) {
@@ -54,6 +55,15 @@ class GameScene extends Phaser.Scene {
     // create a group for the aliens
     this.alienGroup = this.add.group()
     this.createAlien()
+
+    // Collisions between missiles and aliens
+    this.physics.add.collider(this.missileGroup, this.alienGroup, function (missileCollide, alienCollide) {
+      alienCollide.destroy()
+      missileCollide.destroy()
+      this.sound.play('explosion')
+      this.createAlien()
+      this.createAlien()
+    }.bind(this))
   }
 
   update (time, delta) {
